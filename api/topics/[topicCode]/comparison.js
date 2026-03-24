f2b4a912072fb169966e0c9258aa50fde84b1124
MandarinExpert <zhangfengmin532@163.com>
2026-03-24 13:39:11 +0800

    fix: reduce Vercel serverless function count

--- api/topics/[topicCode]/comparison.js
+++
@@ -1,6 +0,0 @@
-import { getTopicComparison } from "../../../server/content-engine.js";
-
-export default function handler(req, res) {
-  const topicCode = Array.isArray(req.query?.topicCode) ? req.query.topicCode[0] : req.query?.topicCode;
-  res.status(200).json({ entries: getTopicComparison(topicCode || "") });
-}
